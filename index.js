a05be2a5a1fb22a1dc4355d07238fbbb94e10ac0
Kluddizz <flhansen.93@gmail.com>
2020-05-07 23:09:10 +0200

    Create route for login

--- index.js
+++ index.js
@@ -2,6 +2,27 @@ const server = require('./server');
 const db = require('./db');
 const port = 5000;
 
+server.post('/auth/login', async (req, res) => {
+	const { username, password } = req.body;
+
+  const query = await db.query(`
+    SELECT *
+    FROM users
+    WHERE username = $1
+          AND password = crypt($2, password)
+    ;
+  `, [username, password]);
+
+  if (query.rows.length == 1) {
+    const user = query.rows[0];
+    const token = server.generateToken({ username: user.username, email: user.email }, 'secret.key');
+
+    res.set(200, 'Authentication successful', { token: token });
+  } else {
+    res.set(403, 'Authentication failed');
+  }
+});
+
 server.listen(port, () => {
 	console.log(`server running on port ${port}...`);
 });
